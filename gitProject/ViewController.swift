1aa8e7c62157cae08d236f622e9a1cd3834ffe98
FaNtast2912 <z_maks@inbox.ru>
2024-11-08 19:32:52 +0300

    properties added

--- gitProject/ViewController.swift
+++ gitProject/ViewController.swift
@@ -8,10 +8,14 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // TO DO
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // TO DO
     }
 
 
