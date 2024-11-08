908130cf440bc36d527a28c5008f3bdaba2916d5
FaNtast2912 <z_maks@inbox.ru>
2024-11-08 19:17:37 +0300

    initial commit

---
+++ gitProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  gitProject
+//
+//  Created by Maksim Zakharov on 08.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ gitProjectTests/gitProjectTests.swift
@@ -0,0 +1,17 @@
+//
+//  gitProjectTests.swift
+//  gitProjectTests
+//
+//  Created by Maksim Zakharov on 08.11.2024.
+//
+
+import Testing
+@testable import gitProject
+
+struct gitProjectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
